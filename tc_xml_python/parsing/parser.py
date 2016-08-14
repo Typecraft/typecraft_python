348e104160ae950f9f524209b4a5e907e391ae93
Tormod Haugland <tormod.haugland@gmail.com>
2016-08-14 14:03:07 +0200

    Added empty shell of parser

---
+++ tc_xml_python/parsing/parser.py
@@ -0,0 +1,27 @@
+
+class Parser:
+    """
+    This class contains functionality for parsing Typecraft-xml files
+    into Text objects.
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def parse(string):
+        """
+        Will parse a Typecraft-xml string into a list of Text objects.
+
+        :param string:
+        :return:
+        """
+        pass
+
+    @staticmethod
+    def parseFile(file):
+        """
+        Will parse a Typecraft-xml file into a list of Text objects.
+        :param file:
+        :return:
+        """
+
