82d1950b1920d1a7fcb7708e278fcd1bfc2ab0a3
Tormod Haugland <tormod.haugland@gmail.com>
2016-08-15 09:05:36 +0200

    Bump version: 0.1.1 → 0.1.1

--- tc_xml_python/__init__.py
+++ tc_xml_python/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Tormod Haugland'
 __email__ = 'tormod.haugland@gmail.com'
-__version__ = '0.1.1'
+__version__ = '__version__ = '0.1.1''
 
 
 __all__ = ['globals', 'models', 'parsing', 'exceptions']
