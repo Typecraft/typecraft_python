ae5d1105ca1e89da5be07e4d9d82847a84022745
Tormod Haugland <tormod.haugland@gmail.com>
2016-08-22 15:44:39 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='tc_xml_python',
-    version='0.3.0',
+    version='0.3.1',
     description="Lightweight implementation of the Typecraft XML format in python.",
     long_description=readme + '\n\n' + history,
     author="Tormod Haugland",
