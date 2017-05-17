905b6626327d83c912bb62b6b6243db7a0a63329
Tormod Haugland <tormod.haugland@gmail.com>
2017-05-17 22:23:58 +0200

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='typecraft_python',
-    version='0.5.0',
+    version='0.5.1',
     description="Lightweight implementation of the Typecraft XML format in python.",
     long_description=readme + '\n\n' + history,
     author="Tormod Haugland",
--- typecraft_python/__init__.py
+++ typecraft_python/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Tormod Haugland'
 __email__ = 'tormod.haugland@gmail.com'
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 
 __all__ = ['globals', 'models', 'parsing', 'exceptions']
