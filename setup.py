43115eccfb52c6aa915d4932079c958659021729
Tormod Haugland <tormod.haugland@gmail.com>
2018-04-27 13:10:32 +0200

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='typecraft_python',
-    version='0.8.0',
+    version='0.9.0',
     description="Lightweight implementation of the Typecraft XML format in python.",
     long_description=readme + '\n\n' + history,
     author="Tormod Haugland",
